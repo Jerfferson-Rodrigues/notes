30fbd202c06270e4e354be3824b6df2437f8a5f3
Jerfferson-Rodrigues <67125687+Jerfferson-Rodrigues@users.noreply.github.com>
2024-02-07 17:18:49 -0300

    new card add,dialog, portal de notas

--- src/app.tsx
+++ src/app.tsx
@@ -2,6 +2,11 @@ import logo from "./assets/logo-nlw.svg";
 import { NewNoteCard } from "./components/NewNoteCard";
 import { NoteCard } from "./components/note-card";
 
+const note = {
+  date: new Date(),
+  content: "Confie, Vai dá certo!",
+};
+
 export function App() {
   return (
     <div className="mx-auto max-w-6xl my-12 space-y-6">
@@ -19,10 +24,7 @@ export function App() {
 
       <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
         <NewNoteCard />
-        <NoteCard />
-        <NoteCard />
-        <NoteCard />
-        <NoteCard />
+        <NoteCard note={note} />
       </div>
     </div>
   );
